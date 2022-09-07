83f05bdf6c99918e49296b0dbce993d302a37177
DinisDimitris <d.dinisdimitris@gmail.com>
2022-09-07 16:13:16 +0100

    Tile generation now supports x by y resolution. The problem was that the model matrix was placing the tiles which were size of (ScreenSize / 8) on a 8 by 8 board. Now the tiles are being passed in the vertex shader on a x * ScreenSize/ 8 where x > 8.

--- Chess/Renderer.cs
+++ Chess/Renderer.cs
@@ -23,8 +23,6 @@ namespace Engine.Render
         private int _vertexArrayObject;
 
         private Shader _shader;
-
-        private Matrix4 _model;
         private Matrix4 _view;
         private Matrix4 _projection;
 
@@ -41,10 +39,10 @@ namespace Engine.Render
 
             var vertices = new float[]
             {
-                Size.X / 100.0f,  Size.Y / 100.0f, 0.0f, // top right
-                Size.X / 100.0f, -Size.Y / 100.0f, 0.0f, // bottom right
-                -Size.X / 100.0f, -Size.Y / 100.0f, 0.0f, // bottom left
-                -Size.X / 100.0f,  Size.Y / 100.0f, 0.0f, // top left
+                Size.X / 8.0f,  Size.Y / 8.0f, 0.0f, // top right
+                Size.X / 8.0f, -Size.Y / 8.0f, 0.0f, // bottom right
+                -Size.X / 8.0f, -Size.Y / 8.0f, 0.0f, // bottom left
+                -Size.X / 8.0f,  Size.Y / 8.0f, 0.0f, // top left
             };
 
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -67,9 +65,9 @@ namespace Engine.Render
             _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
             _shader.Use();
 
-            _view = Matrix4.CreateTranslation(Size.X / 100.0f, Size.Y / 100.0f, -0.0005f);
+            _view = Matrix4.CreateTranslation(Size.X / 8 , Size.Y / 8, -0.0005f);
 
-            _projection = Matrix4.CreateOrthographicOffCenter(0.0f, Size.X / 100.0f, 0.0f, Size.Y / 100.0f, -0.1f, 1.0f);
+            _projection = Matrix4.CreateOrthographicOffCenter(0.0f, Size.X, 0.0f, Size.Y , -0.1f, 1.0f);
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
@@ -78,13 +76,10 @@ namespace Engine.Render
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            _model = Matrix4.Identity;
 
             _shader.Use();
 
             GL.BindVertexArray(_vertexArrayObject);
-
-            _shader.SetMatrix4("model", _model);
             _shader.SetMatrix4("view", _view);
             _shader.SetMatrix4("projection", _projection);
 
@@ -97,19 +92,17 @@ namespace Engine.Render
 
         public void RenderFromBoardModel(Structures.Tile[,] board)
         {
-
             for (int x = 0; x < board.GetLength(0); x++)
             {
                 for (int y = 0; y < board.GetLength(1); y++)
                 {
-                    _shader.SetMatrix4("model", board[x, y].Identity);
+                    _shader.SetVector3("offset", new Vector3(x * Size.X / 8,y * Size.Y / 8, 1));
                     _shader.SetVector4("tileColour", board[x, y].Color);
 
                     GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
                 }
             }
-            }
-
+        }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
@@ -133,17 +126,18 @@ namespace Engine.Render
 
             var vertices = new float[]
             {
-                Size.X / 100.0f,  Size.Y / 100.0f, 0.0f, // top right
-                Size.X / 100.0f, -Size.Y / 100.0f, 0.0f, // bottom right
-                -Size.X / 100.0f, -Size.Y / 100.0f, 0.0f, // bottom left
-                -Size.X / 100.0f,  Size.Y / 100.0f, 0.0f, // top left
+                Size.X / 8.0f,  Size.Y / 8.0f, 0.0f, // top right
+                Size.X / 8.0f, -Size.Y / 8.0f, 0.0f, // bottom right
+                -Size.X / 8.0f, -Size.Y / 8.0f, 0.0f, // bottom left
+                -Size.X / 8.0f,  Size.Y / 8.0f, 0.0f, // top left
             };
 
             GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
 
-            _view = Matrix4.CreateTranslation(Size.X / 100.0f, Size.Y / 100.0f, -0.0005f);
 
-            _projection = Matrix4.CreateOrthographicOffCenter(0.0f, Size.X / 100.0f, 0.0f, Size.Y / 100.0f, -0.1f, 1.0f);
+            _view = Matrix4.CreateTranslation(Size.X / 8 , Size.Y / 8, -0.0005f);
+
+            _projection = Matrix4.CreateOrthographicOffCenter(0.0f, Size.X, 0.0f, Size.Y , -0.1f, 1.0f);
 
             GL.Viewport(0, 0, Size.X, Size.Y);
         }
--- Chess/Shaders/shader.vert
+++ Chess/Shaders/shader.vert
@@ -2,13 +2,13 @@
 
 layout(location = 0) in vec3 aPosition;
 
-uniform mat4 model;
-
 uniform mat4 view;
 
 uniform mat4 projection;
 
+uniform vec3 offset;
+
 void main(void)
 {
-    gl_Position = vec4(aPosition, 1.0) * model * view * projection;
+    gl_Position = vec4(aPosition.x + offset.x, aPosition.y + offset.y, aPosition.z, 1.0) * view * projection;
 }
