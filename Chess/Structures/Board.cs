6f35308e2d8fa5764ed56e002cee0ca2c699816e
DinisDimitris <d.dinisdimitris@gmail.com>
2024-02-16 21:23:37 +0000

    adding resources for setting up pieces on board

--- Chess/Structures/Board.cs
+++ Chess/Structures/Board.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using OpenTK.Mathematics;
 
@@ -9,10 +10,9 @@ namespace Structures
         {
             var board = new Tile[8, 8];
 
+            var textCordsByTexturePath = GetStartingPositionTextures();
+
             Vector4 color;
-
-            string texture = "";
-
             for (int x = 0; x < 8; x++)
             {
                 for (int y = 0; y < 8; y++)
@@ -22,18 +22,19 @@ namespace Structures
                     //whites
                     if ((y % 2 == 0 && x % 2 == 0) | (y % 2 == 1 && x % 2 == 1))
                     {
-
                         color = new Vector4(1, 1, 1, 1);
-
-                        texture = "Textures/bishop1.png";
-
                     }
                     else
                     {
-                       texture = "";
                         color = new Vector4(0, 0, 0, 0);
                     }
 
+                    string texture = "";
+                    if (textCordsByTexturePath.TryGetValue(position, out string textPath))
+                        texture = "Textures/" + textPath;
+
+
+                    Console.WriteLine(texture);
                     board[x, y] = new Tile(position, color, texture);
                 }
             }
@@ -41,10 +42,39 @@ namespace Structures
             return board;
         }
 
-        public static List<Vector2> GetStartingPositionTextures()
+        public static Dictionary<Vector2, string> GetStartingPositionTextures()
         {
-            return new List<Vector2>();
+            Dictionary<Vector2, string> startingPositions = new Dictionary<Vector2, string>
+            {
+                {new Vector2(0, 0), "rook.png" }, // Rook
+                {new Vector2(1, 0), "knight.png" }, // Knight
+                {new Vector2(2, 0), "bishop.png" }, // Bishop
+                {new Vector2(3, 0), "queen.png" }, // Queen
+                {new Vector2(4, 0), "king.png" }, // King
+                {new Vector2(5, 0), "bishop.png" }, // Bishop
+                {new Vector2(6, 0), "knight.png" }, // Knight
+                {new Vector2(7, 0), "rook.png" }, // Rook
+                {new Vector2(0, 7), "rook1.png"},
+                {new Vector2(1, 7), "knight1.png"},
+                {new Vector2(2, 7), "bishop1.png"},
+                {new Vector2(3, 7), "queen1.png"},
+                {new Vector2(4, 7), "king1.png"},
+                {new Vector2(5, 7), "bishop1.png"},
+                {new Vector2(6, 7), "knight1.png"},
+                {new Vector2(7, 7), "rook1.png"}
+            };
+            for (int i = 0; i < 8; i++) // White Pawns
+            {
+                startingPositions.Add(new Vector2(i, 1), "pawn.png");
+            }
 
+            for (int i = 0; i < 8; i++) // Black Pawns
+            {
+                startingPositions.Add(new Vector2(i, 6), "pawn1.png");
+            }
+
+            return startingPositions;
         }
+
     }
 }
